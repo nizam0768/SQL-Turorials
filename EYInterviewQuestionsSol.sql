017cf8a0bb42ccbc39819b027703f4ba8a82fea4
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-09-25 00:16:02 +0530

    Solution to problem

--- EYInterviewQuestionsSol.sql
+++ EYInterviewQuestionsSol.sql
@@ -25,3 +25,62 @@ Select * from
 	DENSE_RANK() Over (Order by salary DESC) AS salary_rank 
 	from employees) ranked 
 	Where salary_rank = 2
+
+GO
+
+-- Q: write a SQL query that calculates the average monthly revenue from each sector in 2020.
+
+WITH Monthly AS (
+    SELECT 
+        MONTH(transaction_date) AS transaction_month,
+        company_id,
+        AVG(revenue) AS avg_revenue
+    FROM transactions
+    WHERE YEAR(transaction_date) = 2020
+    GROUP BY MONTH(transaction_date), company_id
+)
+
+SELECT 
+    m.transaction_month AS month,
+    s.sector,
+    AVG(m.avg_revenue) AS avg_revenue
+FROM Monthly m
+JOIN sectors s ON m.company_id = s.company_id
+GROUP BY m.transaction_month, s.sector
+ORDER BY m.transaction_month, s.sector;
+
+
+GO
+-- Q: Analyzing Consulting Project Performance
+
+--Part1: The total billing amount for each project.
+SELECT 
+	p.project_name,
+	SUM(b.billing_amount) AS total_billing_amount
+	FROM projects p
+	JOIN billing b ON p.project_id = b.project_id
+	WHERE b.billing_date BETWEEN p.project_start_date AND p.project_end_date
+	GROUP BY p.project_name;
+
+GO
+
+--Part2: The average monthly billing amount for each project.
+
+WITH monthly AS (
+    SELECT 
+        b.project_id,
+        MONTH(b.billing_date) AS transaction_month,
+        AVG(b.billing_amount) AS avg_billing_amount
+    FROM billing b
+    JOIN projects p ON b.project_id = p.project_id
+    WHERE b.billing_date BETWEEN p.project_start_date AND p.project_end_date
+    GROUP BY b.project_id, MONTH(b.billing_date)
+)
+
+SELECT 
+    p.project_name,
+    m.transaction_month,
+    m.avg_billing_amount AS avg_billing
+FROM monthly m
+JOIN projects p ON m.project_id = p.project_id
+ORDER BY p.project_name, m.transaction_month;
